8f5ff34bf291d2dd9de6e8737a83d035ab5be36e
lwedge99 <116623885+lwedge99@users.noreply.github.com>
2024-10-11 10:59:21 +0800

    ignore prune args if not full mode

--- crates/node/core/src/args/pruning.rs
+++ crates/node/core/src/args/pruning.rs
@@ -126,6 +126,8 @@ impl PruningArgs {
                     receipts_log_filter: Default::default(),
                 },
             }
+        } else {
+            return None;
         }
 
         // Override with any explicitly set prune.* flags.
