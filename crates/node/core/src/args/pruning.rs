0c0c7b39758dd300cf9afabb8f2e8935478103fa
lwedge99 <116623885+lwedge99@users.noreply.github.com>
2025-09-16 15:02:57 +0800

    migrate sentio changes

--- crates/node/core/src/args/pruning.rs
+++ crates/node/core/src/args/pruning.rs
@@ -135,6 +135,8 @@ impl PruningArgs {
                     receipts_log_filter: Default::default(),
                 },
             }
+        } else {
+            return None;
         }
 
         // Override with any explicitly set prune.* flags.
